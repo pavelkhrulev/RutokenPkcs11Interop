a4dab847eb30d262784b8e20165ba7866216d6b9
Pavel Khrulev <pahan4igg@gmail.com>
2017-05-04 14:29:06 +0600

    Added RSA signature tests

--- src/RutokenPkcs11InteropTests/HighLevelAPI41/_HL41_21_SignAndVerifyTest.cs
+++ src/RutokenPkcs11InteropTests/HighLevelAPI41/_HL41_21_SignAndVerifyTest.cs
@@ -111,5 +111,57 @@ namespace RutokenPkcs11InteropTests.HighLevelAPI41
                 }
             }
         }
+
+        /// <summary>
+        /// C_SignInit, C_Sign, C_VerifyInit and C_Verify test.
+        /// </summary>
+        [TestMethod]
+        public void _HL41_21_03_SignAndVerify_RSA_Test()
+        {
+            if (Platform.UnmanagedLongSize != 4 || Platform.StructPackingSize != 1)
+                Assert.Inconclusive("Test cannot be executed on this platform");
+
+            using (Pkcs11 pkcs11 = new Pkcs11(Settings.Pkcs11LibraryPath, Settings.UseOsLocking))
+            {
+                // Установление соединения с Рутокен в первом доступном слоте
+                Slot slot = Helpers.GetUsableSlot(pkcs11);
+
+                // Открытие RW сессии
+                using (Session session = slot.OpenSession(false))
+                {
+                    // Выполнение аутентификации пользователя
+                    session.Login(CKU.CKU_USER, Settings.NormalUserPin);
+
+                    // Инициализация хэш-функции
+                    Mechanism digestMechanism = new Mechanism(CKM.CKM_SHA_1);
+
+                    byte[] sourceData = TestData.Digest_Gost3411_SourceData;
+
+                    // Вычисление хэш-кода данных
+                    byte[] digest = session.Digest(digestMechanism, sourceData);
+
+                    // Генерация ключевой пары RSA
+                    ObjectHandle publicKey = null;
+                    ObjectHandle privateKey = null;
+                    Helpers.GenerateRSAKeyPair(session, out publicKey, out privateKey, Settings.RsaKeyPairId);
+
+                    // Инициализация операции подписи данных по алгоритму RSA
+                    Mechanism signMechanism = new Mechanism(CKM.CKM_RSA_PKCS);
+
+                    // Подпись данных
+                    byte[] signature = session.Sign(signMechanism, privateKey, digest);
+
+                    // Проверка подписи для данных
+                    bool isValid = false;
+                    session.Verify(signMechanism, publicKey, digest, signature, out isValid);
+
+                    Assert.IsTrue(isValid);
+
+                    session.DestroyObject(privateKey);
+                    session.DestroyObject(publicKey);
+                    session.Logout();
+                }
+            }
+        }
     }
 }
