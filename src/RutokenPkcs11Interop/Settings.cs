c7ee1d5318899a832d74cb764e2614eec75c520f
Pavel Khrulev <pahan4igg@gmail.com>
2017-05-07 13:55:56 +0600

    Update library version

---
+++ src/RutokenPkcs11Interop/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace RutokenPkcs11Interop
+{
+    public static class Settings
+    {
+        private static string _rutokenEcpDllName = @"rtPKCS11ECP.dll";
+
+        public static string RutokenEcpDllDefaultPath =>
+            Path.Combine(Environment.SystemDirectory, _rutokenEcpDllName);
+
+        public static bool OsLockingDefault => true;
+    }
+}
