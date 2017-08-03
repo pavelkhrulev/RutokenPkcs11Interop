ba9d95f9c9d2e12229ed08f95e3764e0134b7608
Pavel Khrulev <pahan4igg@gmail.com>
2017-08-03 13:14:28 +0300

    Changed .Net version to 4.5

--- src/RutokenPkcs11Interop/Properties/AssemblyInfo.cs
+++ src/RutokenPkcs11Interop/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RutokenPkcs11Interop")]
-[assembly: AssemblyDescription("Rutoken Api")]
+[assembly: AssemblyDescription("Rutoken Interop Api")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aktiv")]
 [assembly: AssemblyProduct("RutokenPkcs11Interop")]
