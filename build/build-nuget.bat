5db2a30e07a9966ee06ef43da0f401d7505e2fe6
Pavel Khrulev <pahan4igg@gmail.com>
2019-03-20 22:58:49 +0300

    #23 Update build scripts for building nuget with custom Pkcs11Interop

--- build/build-nuget.bat
+++ build/build-nuget.bat
@@ -2,10 +2,10 @@
 
 @rem Delete output directory
 rmdir /S /Q nuget
+SET Pkcs11InteropDir=..\..\Pkcs11Interop\build
 
 @rem Create output directories
 mkdir nuget\lib\net45 || goto :error
-@rem mkdir nuget\lib\netstandard1.3 || goto :error
 mkdir nuget\lib\netstandard2.0 || goto :error
 mkdir nuget\lib\monoandroid2.3 || goto :error
 mkdir nuget\lib\xamarinios1.0 || goto :error
@@ -13,11 +13,15 @@ mkdir nuget\lib\xamarinmac2.0 || goto :error
 
 @rem Copy assemblies to output directories
 copy net45\RutokenPkcs11Interop.dll nuget\lib\net45 || goto :error
-@rem copy netstandard1.3\RutokenPkcs11Interop.dll nuget\lib\netstandard1.3 || goto :error
+copy %Pkcs11InteropDir%\net45\Pkcs11Interop.dll nuget\lib\net45 || goto :error
 copy netstandard2.0\RutokenPkcs11Interop.dll nuget\lib\netstandard2.0 || goto :error
+copy %Pkcs11InteropDir%\netstandard2.0\Pkcs11Interop.dll nuget\lib\netstandard2.0 || goto :error
 copy monoandroid2.3\RutokenPkcs11Interop.dll nuget\lib\monoandroid2.3 || goto :error
+copy %Pkcs11InteropDir%\monoandroid2.3\Pkcs11Interop.dll nuget\lib\monoandroid2.3 || goto :error
 copy xamarinios1.0\RutokenPkcs11Interop.dll nuget\lib\xamarinios1.0 || goto :error
+copy %Pkcs11InteropDir%\xamarinios1.0\Pkcs11Interop.dll nuget\lib\xamarinios1.0 || goto :error
 copy xamarinmac2.0\RutokenPkcs11Interop.dll nuget\lib\xamarinmac2.0 || goto :error
+copy %Pkcs11InteropDir%\xamarinmac2.0\Pkcs11Interop.dll nuget\lib\xamarinmac2.0 || goto :error
 
 @rem Create package
 copy RutokenPkcs11Interop.nuspec nuget || goto :error
