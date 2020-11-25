4f7003393bc6b838930393a8c95e86f13989e5e9
lo1ol <mkh199740@mail.ru>
2020-11-25 14:07:03 +0300

    move PinPolicy to common

--- src/RutokenPkcs11Interop/RutokenPkcs11InteropTests/HighLevelAPI/_HL_35_PinPolicyTest.cs
+++ src/RutokenPkcs11Interop/RutokenPkcs11InteropTests/HighLevelAPI/_HL_35_PinPolicyTest.cs
@@ -2,6 +2,7 @@
 using Net.Pkcs11Interop.Common;
 using NUnit.Framework;
 using Net.Pkcs11Interop.HighLevelAPI;
+using RutokenPkcs11Interop.Common;
 using RutokenPkcs11Interop.HighLevelAPI;
 
 namespace RutokenPkcs11InteropTests.HighLevelAPI
