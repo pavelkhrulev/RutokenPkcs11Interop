c7ee3552718e8ac070d02109df507ae6ee7c0d41
lo1ol <mkh199740@mail.ru>
2020-12-09 15:30:18 +0300

    get package version from assembly info

--- build/build-nuget.bat
+++ build/build-nuget.bat
@@ -17,9 +17,22 @@ copy monoandroid2.3\RutokenPkcs11Interop.dll nuget\lib\monoandroid2.3 || goto :e
 copy xamarinios1.0\RutokenPkcs11Interop.dll nuget\lib\xamarinios1.0 || goto :error
 copy xamarinmac2.0\RutokenPkcs11Interop.dll nuget\lib\xamarinmac2.0 || goto :error
 
+@rem prepare spec file
+set dllfile= nuget\lib\net45\RutokenPkcs11Interop.dll
+powershell -NoLogo -NoProfile -Command (Get-Item %dllfile%).VersionInfo.FileVersion > tmpFile || goto :error
+set /p version= < tmpFile
+powershell -NoLogo -NoProfile -Command (Get-Item %dllfile%).VersionInfo.LegalCopyright > tmpFile || goto :error
+set /p copyright= < tmpFile
+
+del tmpFile
+
+set tmplfile=RutokenPkcs11Interop.nuspec.tmpl
+set nuspecfile=RutokenPkcs11Interop.nuspec
+powershell -Command "(Get-Content %tmplfile%).replace('VERSION', '%version%').replace('COPYRIGHT', '%copyright%') | Set-Content %nuspecfile%" || goto :error
+
 @rem Create package
-copy RutokenPkcs11Interop.nuspec nuget || goto :error
-nuget pack nuget\RutokenPkcs11Interop.nuspec || goto :error
+move %nuspecfile% nuget || goto :error
+nuget pack nuget\%nuspecfile% || goto :error
 
 @echo *** BUILD NUGET SUCCESSFUL ***
 @endlocal
